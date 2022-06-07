c16396b39906ce24685cb91f3f9264bde462f256
Denis Ognev <den.is.ognev@yandex.ru>
2022-06-08 00:57:25 +0300

    Start work on DB interface

---
+++ src/bot/models/BD.py
@@ -0,0 +1,24 @@
+import sqlite3 as sl
+import ABC
+
+SQLLITE_DB_PATH = 'test.db'
+
+# con = sl.connect(SQLLITE_DB_PATH)
+
+
+# with con:
+#     con.execute("""
+#         CREATE TABLE USER (
+#             id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+#             name TEXT,
+#             age INTEGER
+#         );
+
+
+
+class DBInterface(ABC):
+    
+
+
+if __name__ == "__main__":
+    
